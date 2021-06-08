8f11abff26179874ee2463b404de87e9b5ec4aaa
Paul A. Swarthout <paul@paulswarthout.com>
2021-06-08 15:10:08 -0500

    Updated plugin version to 2.2.1.
    Tested plugin with PHP 8.0 on WordPress 5.7.2
    
    Added namespace child_themes_helper; to all PHP files.
    
    Added the 'if ( ! defined('ABSPATH' ) ) exit;' line to all PHP classes.
    
    Updated wp_enqueue_script() calls to properly load the scripts at the bottom of the page. Also, now uses the plugin version instead of the WordPress version to determine whether to avoid the cached copy.
    
    Removed calls to activeThemes->fixDelimiter() and replaced with calls to wp_normalize_path().
    
    I used to use EditPlus as my development editor. It was handy because 1) I could save files directly to the webserver via FTP, and 2) I could specify whether I wanted Unix/Linux line-endings, or Windows line-endings. Recently (a year ago, now), after an update, I could no longer specify what kind of line-endings I wanted the file to have. Instead, EditPlus took over that duty and line-endings were automatically specified based upon the operating system that I was saving the file from. As a result, many of my code files appeared to be double-spaced because files with Linux line-endings of '\n\n' were converted as Windows line-endings (because I was working on a Windows computer) to '\r\n\r\n'. For every 100 line file, EditPlus wrote them out as a 200 line file, for example. Now I use Visual Studio Code and choose to fix the files as I find them. All of the files in Child Themes Helper have been fixed.

--- js/menu.js
+++ js/menu.js
@@ -1,167 +1,167 @@
-Number.prototype.px = function () {
-	return this + "px";
-}
-function elementInfo(element) {
-	this.element= element;
-
-	this.rect = element.getBoundingClientRect(element);
-
-	this.width	= this.rect.width;
-	this.height	= this.rect.height;
-
-	this.UL = 
-		{
-			'X'	:	this.rect.left,
-			'Y' :	this.rect.top
-		};
-	this.UR = 
-		{
-			'X' :	this.rect.right,
-			'Y' :	this.rect.top
-		};
-	this.LL = 
-		{
-			'X' :	this.rect.left,
-			'Y' :	this.rect.bottom
-		};
-	this.LR = 
-		{
-			'X' :	this.rect.right,
-			'Y' :	this.rect.bottom
-		};
-	this.isMouseOver = function (point = mousePosition) {
-		var obj = this;
-		if ((point.x >= obj.UL.X && point.x <= obj.UR.X) &&
-			(point.y >= obj.UL.Y && point.y <= obj.LL.Y)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-}
-var pas_cth_menu =
-	[
-		{'title'	:	'Copy File to Child Theme',		'function'	:	'pas_cth_js_selectFile', 'themeType'	: 'parent'},
-		{'title'	:	'Remove File from Child Theme', 'function'	:	'pas_cth_js_selectFile', 'themeType'	: 'child' },
-		{'title'	:	'Edit Child Theme File',		'function'	:	'pas_cth_js_editFile',	 'themeType'	: 'child' },
-		{'title'	:	'View Parent Theme File',		'function'	:	'pas_cth_js_editFile',	 'themeType'	: 'parent'}
-	];
-window.addEventListener("click", function (e) {
-	if (document.getElementById("popupMenu") == null) { return; }
-	var popupMenu = document.getElementById("popupMenu");
-	var position = { 'x'	:	e.clientX,
-					 'y'	:	e.clientY };
-	var obj = new elementInfo(popupMenu);
-	if (obj.isMouseOver(position)) {
-		return;
-	} else {
-		if (popupMenu.parentNode != null) {
-			popupMenu.parentNode.removeChild(popupMenu);
-		}
-		popupMenu.remove();
-	}
-	return;
-});
-function copyDataAttributes(targetElement, sourceElement) {
-	var ndx, attribute;
-	for (ndx = 0; ndx < sourceElement.attributes.length; ndx++) {
-		attribute = sourceElement.attributes[ndx];
-		if (attribute.name.left(5).toLowerCase() == "data-") {
-			targetElement.setAttribute(attribute.nodeName, attribute.nodeValue);
-		}
-	}
-}
-function pas_cth_js_openMenu(element, event) {
-	if (event.shiftKey) { return }
-	var pos = new elementInfo(element)
-	var position = { 'x'	:	event.clientX, 'y'	:	event.clientY };
-	var jsdata = JSON.parse(element.getAttribute("data-jsdata"));
-	var elementID = Date.now(); // clock ticks since midnight at the epoch. Guarantees a unique ID number.
-	var childGrid = document.getElementById("childGrid");
-	var parentGrid = document.getElementById("parentGrid");
-	var themeType = "";
-
-	var obj = new elementInfo(childGrid);
-	if (obj.isMouseOver(position)) {
-		themeType = "child";
-	} else {
-		obj = new elementInfo(parentGrid);
-		if (obj.isMouseOver(position)) {
-			themeType = "parent";
-		}
-	}
-	if (! themeType.length) {
-		return;
-	}
-	var menuElements = [];
-	pas_cth_menu.forEach(function (cell) {
-		if (cell.themeType == themeType) {
-			menuElements[menuElements.length] = cell;
-		}
-	});
-	element.setAttribute("id", "file_" + elementID);
-
-	var box = document.getElementById("popupMenu");
-	if (box != null) {
-		box.parentNode.removeChild(box);
-		box.remove();
-	}
-	box = document.createElement("DIV");
-	var p = document.createElement("P");
-	p.id = "menuFileName";
-	var txt = document.createTextNode("File: " + jsdata.file);
-	p.appendChild(txt);
-
-	
-	
-	var rect = p.getBoundingClientRect();
-	var pHeight = rect.height;
-	rect = p.getBoundingClientRect();
-
-	box.appendChild(p);
-
-	var anchor = [];
-
-	for (var ndx = 0; ndx < menuElements.length; ndx++) {
-		p = document.createElement("P");
-		p.className = "menuP";
-		anchor[anchor.length] = document.createElement("A");
-		anchor[anchor.length-1].text = menuElements[ndx]['title'];
-		anchor[anchor.length-1].href = "javascript:void(0);";
-		anchor[anchor.length-1].setAttribute("data-elementid", "file_" + elementID);
-		anchor[anchor.length-1].classList.add("popupLinks");
-		anchor[anchor.length-1].onclick = window[menuElements[ndx]['function']];
-		copyDataAttributes(anchor[anchor.length-1], element);
-		p.appendChild(anchor[ndx]);
-		box.appendChild(p);
-	}
-	box.id = "popupMenu";
-
-	var lft = position.x + 25;
-	var tp = position.y;
-	box.style.left = lft.px();
-	box.style.top = tp.px();
-	document.getElementsByTagName("body")[0].appendChild(box);
-
-	box.style.width = box.scrollWidth;
-
-	var pRect = p.getBoundingClientRect();
-	var boxRect = box.getBoundingClientRect();
-
-	event.preventDefault();
-}
-function findCorners(objPos) {
-	this.adjustCorners = function (adjSize) {
-		this.UL.X -= adjSize;
-		this.UL.Y -= adjSize;
-
-		this.UR.X += adjSize;
-		this.UR.Y -= adjSize;
-
-		this.LL.X -= adjSize;
-		this.LL.Y += adjSize;
-
-		this.LR.X += adjSize;
-		this.LR.Y += adjSize;
-	}
-}
+Number.prototype.px = function () {
+	return this + "px";
+}
+function elementInfo(element) {
+	this.element= element;
+
+	this.rect = element.getBoundingClientRect(element);
+
+	this.width	= this.rect.width;
+	this.height	= this.rect.height;
+
+	this.UL = 
+		{
+			'X'	:	this.rect.left,
+			'Y' :	this.rect.top
+		};
+	this.UR = 
+		{
+			'X' :	this.rect.right,
+			'Y' :	this.rect.top
+		};
+	this.LL = 
+		{
+			'X' :	this.rect.left,
+			'Y' :	this.rect.bottom
+		};
+	this.LR = 
+		{
+			'X' :	this.rect.right,
+			'Y' :	this.rect.bottom
+		};
+	this.isMouseOver = function (point = mousePosition) {
+		var obj = this;
+		if ((point.x >= obj.UL.X && point.x <= obj.UR.X) &&
+			(point.y >= obj.UL.Y && point.y <= obj.LL.Y)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+var pas_cth_menu =
+	[
+		{'title'	:	'Copy File to Child Theme',		'function'	:	'pas_cth_js_selectFile', 'themeType'	: 'parent'},
+		{'title'	:	'Remove File from Child Theme', 'function'	:	'pas_cth_js_selectFile', 'themeType'	: 'child' },
+		{'title'	:	'Edit Child Theme File',		'function'	:	'pas_cth_js_editFile',	 'themeType'	: 'child' },
+		{'title'	:	'View Parent Theme File',		'function'	:	'pas_cth_js_editFile',	 'themeType'	: 'parent'}
+	];
+window.addEventListener("click", function (e) {
+	if (document.getElementById("popupMenu") == null) { return; }
+	var popupMenu = document.getElementById("popupMenu");
+	var position = { 'x'	:	e.clientX,
+					 'y'	:	e.clientY };
+	var obj = new elementInfo(popupMenu);
+	if (obj.isMouseOver(position)) {
+		return;
+	} else {
+		if (popupMenu.parentNode != null) {
+			popupMenu.parentNode.removeChild(popupMenu);
+		}
+		popupMenu.remove();
+	}
+	return;
+});
+function copyDataAttributes(targetElement, sourceElement) {
+	var ndx, attribute;
+	for (ndx = 0; ndx < sourceElement.attributes.length; ndx++) {
+		attribute = sourceElement.attributes[ndx];
+		if (attribute.name.left(5).toLowerCase() == "data-") {
+			targetElement.setAttribute(attribute.nodeName, attribute.nodeValue);
+		}
+	}
+}
+function pas_cth_js_openMenu(element, event) {
+	if (event.shiftKey) { return }
+	var pos = new elementInfo(element)
+	var position = { 'x'	:	event.clientX, 'y'	:	event.clientY };
+	var jsdata = JSON.parse(element.getAttribute("data-jsdata"));
+	var elementID = Date.now(); // clock ticks since midnight at the epoch. Guarantees a unique ID number.
+	var childGrid = document.getElementById("childGrid");
+	var parentGrid = document.getElementById("parentGrid");
+	var themeType = "";
+
+	var obj = new elementInfo(childGrid);
+	if (obj.isMouseOver(position)) {
+		themeType = "child";
+	} else {
+		obj = new elementInfo(parentGrid);
+		if (obj.isMouseOver(position)) {
+			themeType = "parent";
+		}
+	}
+	if (! themeType.length) {
+		return;
+	}
+	var menuElements = [];
+	pas_cth_menu.forEach(function (cell) {
+		if (cell.themeType == themeType) {
+			menuElements[menuElements.length] = cell;
+		}
+	});
+	element.setAttribute("id", "file_" + elementID);
+
+	var box = document.getElementById("popupMenu");
+	if (box != null) {
+		box.parentNode.removeChild(box);
+		box.remove();
+	}
+	box = document.createElement("DIV");
+	var p = document.createElement("P");
+	p.id = "menuFileName";
+	var txt = document.createTextNode("File: " + jsdata.file);
+	p.appendChild(txt);
+
+	
+	
+	var rect = p.getBoundingClientRect();
+	var pHeight = rect.height;
+	rect = p.getBoundingClientRect();
+
+	box.appendChild(p);
+
+	var anchor = [];
+
+	for (var ndx = 0; ndx < menuElements.length; ndx++) {
+		p = document.createElement("P");
+		p.className = "menuP";
+		anchor[anchor.length] = document.createElement("A");
+		anchor[anchor.length-1].text = menuElements[ndx]['title'];
+		anchor[anchor.length-1].href = "javascript:void(0);";
+		anchor[anchor.length-1].setAttribute("data-elementid", "file_" + elementID);
+		anchor[anchor.length-1].classList.add("popupLinks");
+		anchor[anchor.length-1].onclick = window[menuElements[ndx]['function']];
+		copyDataAttributes(anchor[anchor.length-1], element);
+		p.appendChild(anchor[ndx]);
+		box.appendChild(p);
+	}
+	box.id = "popupMenu";
+
+	var lft = position.x + 25;
+	var tp = position.y;
+	box.style.left = lft.px();
+	box.style.top = tp.px();
+	document.getElementsByTagName("body")[0].appendChild(box);
+
+	box.style.width = box.scrollWidth;
+
+	var pRect = p.getBoundingClientRect();
+	var boxRect = box.getBoundingClientRect();
+
+	event.preventDefault();
+}
+function findCorners(objPos) {
+	this.adjustCorners = function (adjSize) {
+		this.UL.X -= adjSize;
+		this.UL.Y -= adjSize;
+
+		this.UR.X += adjSize;
+		this.UR.Y -= adjSize;
+
+		this.LL.X -= adjSize;
+		this.LL.Y += adjSize;
+
+		this.LR.X += adjSize;
+		this.LR.Y += adjSize;
+	}
+}
--- js/pasChildThemes.js
+++ js/pasChildThemes.js
@@ -1,89 +1,91 @@
 "use strict;"
-/* pasChildThemes.js
- * This file contains nearly pure JavaScript code.
- */
-
-
-/* pas_cth_js_selectFile() called from an onclick event in ListFolderFiles() in /pasChildThemes.php
- */
+/* pasChildThemes.js
+ * This file contains nearly pure JavaScript code.
+ */
+
 if (typeof(String.prototype.trim) == "undefined") {
 	String.prototype.trim = function () {
 		return this.ltrim().rtrim();
 	}
 }
-function pas_cth_js_selectFile(event) {
-	var element = document.getElementById(event.srcElement.dataset.elementid);
-	var jsInput;
-	var box;
-
-	// requires HTML5 global attribute support for "data-*"
-	jsInput = JSON.parse(element.getAttribute("data-jsdata"));
-	/*
-	 * If selected file is '/functions.php' or '/style.css' display a message, then bail.
-	 */
-	if (jsInput['directory'].length == 0 && (jsInput['file'].toLowerCase() == "style.css" || jsInput['file'].toLowerCase() == "functions.php")) {
-		msg = "<p class='warningHeading'>Action Not Allowed</p><br><br>" +
-		      "Overwriting or deleting a theme's primary stylesheet or functions.php file is not allowed.<br><br>" +
-			  "Click anywhere in this message box to close it.";
-
-		box = pas_cth_js_showBox();
-		box.innerHTML = msg;
-		box.onclick = function () {
-			if (this.parentNode != null) {
-				this.parentNode.removeChild(this);
-			}
-			this.remove();
-		}
-		document.getElementById("pas_cth_actionBox").style.display = "inline";
-		windowFlag = true; // Prevents the window.onclick event from closing this prompt.
-		return;
-	}
-
-	element.setAttribute("data-jsdata", JSON.stringify(jsInput));
-
-	switch (jsInput['themeType'].toLowerCase()) {
-		case "child":
-			jsInput['action'] = 'verifyRemoveFile';
-			element.setAttribute("data-jsdata", JSON.stringify(jsInput));
-			pas_cth_js_removeChildFile(element);
-			break;
-		case "parent":
-			jsInput['action'] = 'verifyCopyFile';
-			element.setAttribute("data-jsdata", JSON.stringify(jsInput));
-			pas_cth_js_copyTemplateFile(element);
-			break;
-	}
-}
 
+/* 
+ * pas_cth_js_selectFile() called from an onclick event in ListFolderFiles() in /pasChildThemes.php
+ */
+function pas_cth_js_selectFile(event) {
+	var element = document.getElementById(event.srcElement.dataset.elementid);
+	var jsInput;
+	var box;
+
+
+	// requires HTML5 global attribute support for "data-*"
+	jsInput = JSON.parse(element.getAttribute("data-jsdata"));
+	/*
+	 * If selected file is '/functions.php' or '/style.css' display a message, then bail.
+	 */
+	if (jsInput['directory'].length == 0 && (jsInput['file'].toLowerCase() == "style.css" || jsInput['file'].toLowerCase() == "functions.php")) {
+		msg = "<p class='warningHeading'>Action Not Allowed</p><br><br>" +
+		      "Overwriting or deleting a theme's primary stylesheet or functions.php file is not allowed.<br><br>" +
+			  "Click anywhere in this message box to close it.";
+
+
+		box = pas_cth_js_showBox();
+		box.innerHTML = msg;
+		box.onclick = function () {
+			if (this.parentNode != null) {
+				this.parentNode.removeChild(this);
+			}
+			this.remove();
+		}
+		document.getElementById("pas_cth_actionBox").style.display = "inline";
+		windowFlag = true; // Prevents the window.onclick event from closing this prompt.
+		return;
+	}
+
+
+	element.setAttribute("data-jsdata", JSON.stringify(jsInput));
+
+
+	switch (jsInput['themeType'].toLowerCase()) {
+		case "child":
+			jsInput['action'] = 'verifyRemoveFile';
+			element.setAttribute("data-jsdata", JSON.stringify(jsInput));
+			pas_cth_js_removeChildFile(element);
+			break;
+		case "parent":
+			jsInput['action'] = 'verifyCopyFile';
+			element.setAttribute("data-jsdata", JSON.stringify(jsInput));
+			pas_cth_js_copyTemplateFile(element);
+			break;
+	}
+}
 function pas_cth_js_showWait() {
-	var body = document.getElementsByTagName("body")[0];
-	body.style.cursor = "wait";
+	pas_cth_spinner.wait_cursor();
+//	var body = document.getElementsByTagName("body")[0];
+//	body.style.cursor = "wait";
 }
 function pas_cth_js_hideWait() {
-	var body = document.getElementsByTagName("body")[0];
-	body.style.cursor = "";
+//	var body = document.getElementsByTagName("body")[0];
+//	body.style.cursor = "";
+	pas_cth_spinner.default_cursor();
 }
 var pas_cth_js_spinTimer = 0;
 var pas_cth_js_spinPosition = 0;
-
 function pas_cth_js_spin() {
-}
-
-
-/* removeChildFile() is called from an onclick event of a button press
- * set up in pas_cth_AJAXFunctions::selectFile()
- * in file '/pasChildThemes.php'
- */
-function pas_cth_js_removeChildFile(element) {
-	var jsInput = JSON.parse(element.getAttribute("data-jsdata"));
-	var dataBlock = {};
+}
 
+/* removeChildFile() is called from an onclick event of a button press
+ * set up in pas_cth_AJAXFunctions::selectFile()
+ * in file '/pasChildThemes.php'
+ */
+function pas_cth_js_removeChildFile(element) {
+	var jsInput = JSON.parse(element.getAttribute("data-jsdata"));
+	var dataBlock = {};
 	pas_cth_js_showWait();
-
+
 	// $_POST[] values in pas_cth_AJAXFunctions::verifyRemoveFile()
 	dataBlock.directory = jsInput['directory'];
 	dataBlock.file		= jsInput['file'];
-
 	var successCallback = function (response) {
 		if (response.length) {
 			pas_cth_js_processResponse(response);
@@ -97,23 +99,20 @@ function pas_cth_js_removeChildFile(element) {
 		pas_cth_js_showBox().innerHTML = msg;
 		pas_cth_js_hideWait();
 	}
-
 	pas_cth_js_AJAXCall(jsInput['action'], dataBlock, successCallback, failureCallback);
-}
-/* pas_cth_js_deleteChildFile() is called from an onclick event in a popup error box
- * set up in pas_cth_AJAXFunctions::verifyRemoveFile() in 'classes/class_ajax_functions.php'
- *
- */
-function pas_cth_js_deleteChildFile(element) {
-	var jsInput = JSON.parse(element.getAttribute("data-jsdata"));
+}
+/* pas_cth_js_deleteChildFile() is called from an onclick event in a popup error box
+ * set up in pas_cth_AJAXFunctions::verifyRemoveFile() in 'classes/class_ajax_functions.php'
+ *
+ */
+function pas_cth_js_deleteChildFile(element) {
+	var jsInput = JSON.parse(element.getAttribute("data-jsdata"));
 	var dataBlock = {};
-
 	pas_cth_js_showWait();
-
-	// $_POST[] values in pas_cth_AJAXFunctions::deleteFile()
+
+	// $_POST[] values in pas_cth_AJAXFunctions::deleteFile()
 	dataBlock.directory = jsInput['directory'];
 	dataBlock.file		= jsInput['file'];
-
 	var successCallback = function (response) {
 		if (response.length) {
 			pas_cth_js_processResponse(response);
@@ -127,9 +126,9 @@ function pas_cth_js_deleteChildFile(element) {
 		pas_cth_js_showBox().innerHTML = msg;
 		pas_cth_js_hideWait();
 	}
-
-	pas_cth_js_AJAXCall(jsInput['action'], dataBlock, successCallback, failureCallback);
-}
+	pas_cth_js_AJAXCall
+(jsInput['action'], dataBlock, successCallback, failureCallback);
+}
 function pas_cth_js_successCallback(response) {
 	if (response.length) {
 		pas_cth_js_processResponse(response);
@@ -143,20 +142,18 @@ function pas_cth_js_failureCallback(status, response) {
 	pas_cth_js_showBox().innerHTML = msg;
 	pas_cth_js_hideWait();
 }
-/*
- * pas_cth_js_copyTemplateFile() responds to an onclick event set up pas_cth_AJAXFunctions::selectFile()
- * in 'classes/class_ajax_functions.php' when a user clicks a file in the template theme files list.
- */
-function pas_cth_js_copyTemplateFile(element) {
-	var jsInput = JSON.parse(element.getAttribute("data-jsdata"));
+/*
+ * pas_cth_js_copyTemplateFile() responds to an onclick event set up pas_cth_AJAXFunctions::selectFile()
+ * in 'classes/class_ajax_functions.php' when a user clicks a file in the template theme files list.
+ */
+function pas_cth_js_copyTemplateFile(element) {
+	var jsInput = JSON.parse(element.getAttribute("data-jsdata"));
 	var dataBlock = {};
-
-	// $_POST[] values in pas_cth_AJAXFunctions::verifyCopyFile()
+
+	// $_POST[] values in pas_cth_AJAXFunctions::verifyCopyFile()
 	dataBlock.directory = jsInput['directory'];
 	dataBlock.file		= jsInput['file'];
-
 	pas_cth_js_showWait();
-
 	var successCallback = function (response) {
 		if (response.length) {
 			pas_cth_js_processResponse(response);
@@ -170,21 +167,18 @@ function pas_cth_js_copyTemplateFile(element) {
 		pas_cth_js_showBox().innerHTML = msg;
 		pas_cth_js_hideWait();
 	}
-
 	pas_cth_js_AJAXCall(jsInput['action'], dataBlock, successCallback, failureCallback);
-}
-function pas_cth_js_overwriteFile(element) {
-	var jsInput = JSON.parse(element.getAttribute("data-jsdata"));
+}
+function pas_cth_js_overwriteFile(element) {
+	var jsInput = JSON.parse(element.getAttribute("data-jsdata"));
 	var dataBlock = {};
-
 	pas_cth_js_showWait();
-
+
 	dataBlock.directory = jsInput['directory'];
 	dataBlock.file		= jsInput['file'];
-
 	// copyFile
 	pas_cth_js_AJAXCall(jsInput['action'], dataBlock, pas_cth_js_successCallback, pas_cth_js_failureCallback);
-}
+}
 function pas_cth_js_resetChildThemeForm(frm) {
 	var formElements = frm.elements, ndx;
 	for (ndx = 0; ndx < formElements.length; ndx++) {
@@ -192,27 +186,24 @@ function pas_cth_js_resetChildThemeForm(frm) {
 	}
 	frm.reset();
 }
-/* The pas_cth_js_createChildTheme() function processes the form in
- * pas_cth_ChildThemesHelper::manage_child_themes() in file 'classes/class_childThemesHelper.php'
- * without actually executing a "Submit" on that form. This prevents the page refresh and allows
- * us to redirect to the admin_url("themes.php") page once the child theme has been created.
- */
+/* The pas_cth_js_createChildTheme() function processes the form in
+ * pas_cth_ChildThemesHelper::manage_child_themes() in file 'classes/class_childThemesHelper.php'
+ * without actually executing a "Submit" on that form. This prevents the page refresh and allows
+ * us to redirect to the admin_url("themes.php") page once the child theme has been created.
+ */
 function pas_cth_js_createChildTheme(element) {
-	var ndx = 0,
+	var ndx = 0,
 		frm = element.form,
 		formElements = frm.elements,
 		dataBlock = {},
 		jsInput,
 		action,
 		e, err = 0;
-
 	pas_cth_js_showWait();
-
 	// Used to validate the data in the form before making the AJAX call to save the data and create the child theme.
 	this.test = function (elementValue = "", elementPattern = "", elementRequiredFlag = false) {
 		var re;
 		var result = false;
-
 		if (elementRequiredFlag) {
 			if (elementValue.length) {
 				if (elementPattern.length) { // element is required, value is not blank, pattern exists, return true if value matches pattern.
@@ -238,9 +229,7 @@ function pas_cth_js_createChildTheme(element) {
 		}
 		return result;
 	}
-
 	var outputMessage = "";
-
 	for (ndx = 0; ndx < formElements.length; ndx++) {
 		e = formElements[ndx];
 		if (! this.test(e.value, e.dataset.pattern, e.required) ) {
@@ -256,21 +245,19 @@ function pas_cth_js_createChildTheme(element) {
 		errorBox.style.overflowY = "scroll";
 		errorBox.style.marginLeft = "-150px";
 		errorBox.style.marginTop = "-150px";
-
 		pas_cth_js_hideWait();
-
 		return;
 	}
 
 
-
-	/* Move the data from the form to the FormData object.
-	 * Data will be accessible using the $_POST[] array in pas_cth_AJAXFunctions::createChildTheme()
-	 * The "action" value, used by wp_ajax_* to target the appropriate PHP function, is an <INPUT>
-	 * element and will get copied to the FormData in the first case of the switch statement below.
-	 */
-	for (ndx = 0; ndx < formElements.length; ndx++) {
-		switch (formElements[ndx].tagName.toUpperCase()) {
+
+	/* Move the data from the form to the FormData object.
+	 * Data will be accessible using the $_POST[] array in pas_cth_AJAXFunctions::createChildTheme()
+	 * The "action" value, used by wp_ajax_* to target the appropriate PHP function, is an <INPUT>
+	 * element and will get copied to the FormData in the first case of the switch statement below.
+	 */
+	for (ndx = 0; ndx < formElements.length; ndx++) {
+		switch (formElements[ndx].tagName.toUpperCase()) {
 			case "INPUT":
 				if (formElements[ndx].name.toLowerCase() == "action") {
 					action = formElements[ndx].value;
@@ -287,7 +274,7 @@ function pas_cth_js_createChildTheme(element) {
 			case "BUTTON":
 				// ignore
 				break;
-		}
+		}
 	}
 	var successCallback = function (response) {
 		if ("SUCCESS:" == response.left("SUCCESS:".length)) {
@@ -298,38 +285,38 @@ function pas_cth_js_createChildTheme(element) {
 		}
 	}
 	// AJAX call to pas_cth_AJAXFunctions::createChildTheme() in 'classes/class_ajax_functions.php'
-	pas_cth_js_AJAXCall(action, dataBlock, successCallback, pas_cth_js_failureCallback);
-}
-// Responds to an onclick event, on a cancel button.
-function pas_cth_js_cancelOverwrite(element) {
-	var box = document.getElementById("pas_cth_actionBox");
-	if (null == box.parentNode) {
-		var theBody = document.getElementsByTagName("body")[0];
-		theBody.removeChild(box);
-	} else {
-		box.parentNode.removeChild(box);
+	pas_cth_js_AJAXCall(action, dataBlock, successCallback, pas_cth_js_failureCallback);
+}
+// Responds to an onclick event, on a cancel button.
+function pas_cth_js_cancelOverwrite(element) {
+	var box = document.getElementById("pas_cth_actionBox");
+	if (null == box.parentNode) {
+		var theBody = document.getElementsByTagName("body")[0];
+		theBody.removeChild(box);
+	} else {
+		box.parentNode.removeChild(box);
 	}
-	pas_cth_js_hideWait();
-}
-// Responds to an onclick event
-function pas_cth_js_cancelDeleteChild(element) {
-	var box = document.getElementById("pas_cth_actionBox");
-	if (null == box.parentNode) {
-		var theBody = document.getElementsByTagName("body")[0];
-		theBody.removeChild(box);
-	} else {
-		box.parentNode.removeChild(box);
+	pas_cth_js_hideWait();
+}
+// Responds to an onclick event
+function pas_cth_js_cancelDeleteChild(element) {
+	var box = document.getElementById("pas_cth_actionBox");
+	if (null == box.parentNode) {
+		var theBody = document.getElementsByTagName("body")[0];
+		theBody.removeChild(box);
+	} else {
+		box.parentNode.removeChild(box);
 	}
-	pas_cth_js_hideWait();
-}
-
-// Responds to an onblur event on the ScreenShot Options page.
-function pas_cth_js_SetOption(element) {
+	pas_cth_js_hideWait();
+}
+
+
+// Responds to an onblur event on the ScreenShot Options page.
+function pas_cth_js_SetOption(element) {
 	var dataBlock = {};
-
+
 	dataBlock.optionName	= element.name;
 	dataBlock.optionValue	= element.value;
-
 	var successCallback = function (response) {
 		alert("Here");
 		if ("SUCCESS:" == response.left("SUCCESS:".length)) {
@@ -339,61 +326,63 @@ function pas_cth_js_SetOption(element) {
 		}
 		pas_cth_js_hideWait();
 	}
-
 	pas_cth_js_AJAXCall('saveOptions', dataBlock, successCallback, pas_cth_js_failureCallback);
-}
-function showChild() {
-	document.getElementById("childGrid").style.display = "inline";
-	document.getElementById("parentGrid").style.display = "none";
-}
-function showParent() {
-	document.getElementById("childGrid").style.display = "none";
-	document.getElementById("parentGrid").style.display = "inline";
-}
-function debugTip(action, msg) {
-	switch (action.toLowerCase()) {
-		case "show":
-			var tipBox = document.createElement("div")
-			tipBox.setAttribute("id", "tipBox")
-			tipBox.setAttribute("class", "tipBox");
-			tipBox.innerHTML = msg;
-
-			tipBox.style.left = (mousePosition.x + 10) + "px";
-			tipBox.style.top  = (mousePosition.y + 10) + "px";
-			document.getElementsByTagName("body")[0].appendChild(tipBox);
-			break;
-		case "hide":
-			kill(document.getElementById("tipBox"));
-			break;
-	}
-}
-function pas_cth_validateField(element = null) {
+}
+function showChild() {
+	document.getElementById("childGrid").style.display = "inline";
+	document.getElementById("parentGrid").style.display = "none";
+}
+function showParent() {
+	document.getElementById("childGrid").style.display = "none";
+	document.getElementById("parentGrid").style.display = "inline";
+}
+function debugTip(action, msg) {
+	switch (action.toLowerCase()) {
+		case "show":
+			var tipBox = document.createElement("div")
+			tipBox.setAttribute("id", "tipBox")
+			tipBox.setAttribute("class", "tipBox");
+			tipBox.innerHTML = msg;
+
+
+			tipBox.style.left = (mousePosition.x + 10) + "px";
+			tipBox.style.top  = (mousePosition.y + 10) + "px";
+			document.getElementsByTagName("body")[0].appendChild(tipBox);
+			break;
+		case "hide":
+			kill(document.getElementById("tipBox"));
+			break;
+	}
+}
+function pas_cth_validateField(element = null) {
 	if (element == null) {
 		return;
 	}
 	var strValue	= element.value.trim();
 	var ptrn		= element.dataset.pattern.trim();
-	if (strValue.length == 0 || ptrn.length == 0) {
-		return;
-	}
-	var re = new RegExp(ptrn);
-	var box;
-	var boxID = "pas_cth_actionBox";
-	var parent = document.getElementsByTagName("body")[0];
-	var onclickFunction = true;
-	var className = "";
-
-	if (! re.test(strValue)) {
-		box = pas_cth_js_createBox(boxID, className, parent, onclickFunction);
-		box.innerHTML = element.dataset.message + "<br><br>Click on this messagebox to close it";
-	}
-}
-function pas_cth_js_launch(btn) {
-	var ajaxAction = btn.getAttribute("data-ajax")
-	var FN = function (response) {
-		if (response.length > 0) {
-
-		}
-	}
-	pas_cth_js_AJAXCall(ajaxAction);
+	if (strValue.length == 0 || ptrn.length == 0) {
+		return;
+	}
+	var re = new RegExp(ptrn);
+	var box;
+	var boxID = "pas_cth_actionBox";
+	var parent = document.getElementsByTagName("body")[0];
+	var onclickFunction = true;
+	var className = "";
+
+
+	if (! re.test(strValue)) {
+		box = pas_cth_js_createBox(boxID, className, parent, onclickFunction);
+		box.innerHTML = element.dataset.message + "<br><br>Click on this messagebox to close it";
+	}
+}
+function pas_cth_js_launch(btn) {
+	var ajaxAction = btn.getAttribute("data-ajax")
+	var FN = function (response) {
+		if (response.length > 0) {
+
+
+		}
+	}
+	pas_cth_js_AJAXCall(ajaxAction);
 }
